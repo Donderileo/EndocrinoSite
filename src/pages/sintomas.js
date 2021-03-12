4069bf0dadd9f3ea5a49ff280b1034b255b4ce98
donderileo <donderileo@gmail.com>
2021-03-12 19:06:04 -0300

    Corrigindo os sintomas

--- src/pages/sintomas.js
+++ src/pages/sintomas.js
@@ -9,37 +9,40 @@ export default function Sintomas() {
 
           <p className="font-bold text-center text-turquesa text-5xl ">Sintomas</p>
 
-          <div className="mt-10 sm:mt-5 grid grid-cols-1 sm:grid-cols-2 gap-6 mx-5">
+          <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 gap-10 sm:mx-20 text-xl text-center">
 
-            <div className="bg-white rounded-xl shadow-xl transform duration-200 hover:-translate-y-2 border-transparent border-2 hover:border-turquesa">
 
-              <p className="break-words text-sm lg:text-lg row-start-1 row-end-2 2xl:text-2xl mt-5 mx-10 sm:text-center  p-5 "> Sempre que houver algum problema relacionado ao metabolismo do seu corpo, ganho ou perda de peso rápidos e sem causa aparente, problemas de crescimento ou quando houver diagnóstico de doenças como diabetes e alterações nos níveis de colesterol ou triglicérides.</p>
-
+            <div className="text-left shadow-xl p-10 rounded-xl flex flex-col justify-center 
+            transform hover:-translate-y-2 hover:shadow-2xl hover:border-turquesa border-4  border-transparent duration-200 ">
+              <p className="font-bold text-xl md:text-2xl text-turquesa">Obesidade</p>
+              <p className="mt-4 text-sm sm:text-lg">Em sua maioria, os casos de obesidade são causados pela má alimentação ou falta de exercícios físicos. Acontece que algumas pessoas podem estar com distúrbios hormonais e, então, acabam por não conseguir perder peso. Neste caso, o médico Endocrinologista fará as investigações necessárias para descobrir porque seu organismo não está “funcionando corretamente”.</p>
             </div>
 
-            <div className="bg-white rounded-xl shadow-xl transform duration-200 hover:-translate-y-2 border-transparent border-2 hover:border-turquesa">
+            <div className="text-left shadow-xl p-10 rounded-xl  flex flex-col justify-center 
+            transform hover:-translate-y-2 hover:shadow-2xl hover:border-turquesa border-4  border-transparent duration-200 ">
 
-              <p className="break-words text-sm lg:text-lg 2xl:text-2xl mt-5 mx-10 sm:text-center  p-5 "> Pessoas que desejam perder peso têm no endocrinologista um aliado importante, uma vez que ele é o profissional que verificará, por meio de exames clínicos e laboratoriais, se todas as glândulas estão funcionando de maneira saudável e se o ganho de peso não está sendo influenciado por alguma alteração hormonal. </p>
-
+              <p className="font-bold text-xl md:text-2xl text-turquesa">Diabetes</p>
+              <p className="mt-4 text-sm sm:text-lg">A insulina é o hormônio produzido pelo pâncreas e que controla a nossa glicemia. Qualquer alteração na produção da insulina provoca o excesso de açúcar no sangue, podendo caracterizar, assim, a diabetes. Se existe histórico familiar, ou se você observar que está com muita sede, vontade de fazer xixi ou perda de peso aparente e sem motivo, é hora de procurar o Endocrinologista.</p>
             </div>
 
+            <div className="text-left shadow-xl p-10 rounded-xl  flex flex-col justify-center 
+            transform hover:-translate-y-2 hover:shadow-2xl hover:border-turquesa border-4  border-transparent duration-200 ">
 
-
-            <div className="bg-white rounded-xl shadow-xl transform duration-200 hover:-translate-y-2 border-transparent border-2 hover:border-turquesa">
-
-              <p className="break-words text-sm lg:text-lg 2xl:text-2xl mt-5 mx-10 sm:text-center  p-5 ">O endocrinologista pode, inclusive, prescrever medicamentos como reguladores de apetite e fazer o acompanhamento e a orientação para o uso correto deste tipo de medicamento.</p>
-
+              <p className="font-bold text-xl md:text-2xl text-turquesa">Distúrbios da menstruação</p>
+              <p className="mt-4 text-sm sm:text-lg">Alterações no ciclo menstrual acontecem com boa parte da população feminina. Se você não passou por alguma, com certeza conhece alguém que já. Elas são caracterizadas por ciclos muito longos, períodos muito curtos e até menstruação desregulada. Todas essas alterações podem ser indicadores de problemas nos hormônios da mulher.</p>
             </div>
 
-            <div className="bg-white rounded-xl shadow-xl transform duration-200 hover:-translate-y-2 border-transparent border-2 hover:border-turquesa">
+            <div className="text-left shadow-xl p-10 rounded-xl  flex flex-col justify-center 
+            transform hover:-translate-y-2 hover:shadow-2xl hover:border-turquesa border-4  border-transparent duration-200 ">
 
-              <p className="break-words text-sm lg:text-lg 2xl:text-2xl mt-5 mx-10 sm:text-center  p-5  col-span-2">Crianças com crescimento deficiente ou excessivo e adultos com alterações repentinas no crescimento de pelos, por exemplo, também podem procurar o endocrinologista para investigação das causas de seus sintomas. </p>
-
+              <p className="font-bold text-xl md:text-2xl text-turquesa">Disfunções tireoidianas</p>
+              <p className="mt-4 text-sm sm:text-lg">Um dos maiores motivos da procura por um Endocrinologista são as disfunções tireoidianas – hipertireoidismo e hipotireoidismo. Funciona assim: a tireoide é uma glândula que deve funcionar em equilíbrio com o cérebro. Este, produz um hormônio chamado TSH que estimula a tireoide a fabricar outros dois hormônios chamados T3 (triiodotironina) e T4 (tiroxina), que desempenham a função de estimular o metabolismo. O hipotireoidismo ou hipertireoidismo se caracteriza, então, quando a tireoide funciona mais lenta ou mais rápida que o normal.</p>
             </div>
-
-
           </div>
+
         </div>
+
+
       </Layout>
     </div >
   )
